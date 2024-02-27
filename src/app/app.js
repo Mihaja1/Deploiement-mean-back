e3f184ceef0f761ea894ac1124478a3b523f4fb5
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2024-02-27 23:24:14 +0300

    feat : listen to port 8000

--- src/app/app.js
+++ src/app/app.js
@@ -6,8 +6,17 @@ const app = express();
 var routes = require('../routes/index');
 
 app.use(cors());
-app.use("/", routes);
 app.use(bodyParser.raw({ type: "application/octet-stream" }));
+app.use("/api", routes);
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 module.exports = app;
