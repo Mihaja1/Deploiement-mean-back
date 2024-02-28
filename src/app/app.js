1c17b418cd10fe038f0c5c970c88f08853f0200a
Mihaja1 <mihajaraman@gmail.com>
2024-02-28 21:29:41 +0300

    Update app.js

--- src/app/app.js
+++ src/app/app.js
@@ -13,7 +13,6 @@ const middleware = new Middleware();
 app.use(cors());
 app.use(bodyParser.raw({ type: "application/octet-stream" }));
 app.use("/api", middleware.checking, routes);
-app.use(errorHandler); // Error handler middleware
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
